1d64dc3dac96f8d2098d34c11f391a7504a49ce8
bimoghanis <bimodarkron@gmail.com>
2021-10-01 17:31:08 +0700

    HelloWorld.java

---
+++ Pratikum 02/HelloWorld.java
@@ -0,0 +1,8 @@
+ public class HelloWorld {
+
+    public static void main(String[] args) {
+	    System.out.println("Nama: Bimo Ghanis Surya Putra Wibowo");
+		System.out.println("NIM: 6706213048");
+		System.out.println("Kelas: 45-04");
+	}
+}
